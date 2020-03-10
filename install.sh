6723e2b1b6bcae162de6f0b03f32b26c60c4037f
drhax9908 <drhax9908@users.noreply.github.com>
2020-03-09 20:35:46 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ cat << EOF > /etc/apache2/mods-enabled/fcgid.conf
 <IfModule mod_fcgid.c>
     FcgidConnectTimeout 120
     IdleTimeout 3600
-    BuysTimeout 300
+    BusyTimeout 300
     IPCCommTimeout 360
     MaxRequestLen 15720640
     <IfModule mod_mime.c>
